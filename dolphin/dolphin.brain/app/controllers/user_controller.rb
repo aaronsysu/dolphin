992549578373d33b946a89aab4aca21ce06e3d7b
aaron <aaronsysu@foxmail.com>
2016-04-03 00:51:21 +0800

    add brain project

---
+++ dolphin/dolphin.brain/app/controllers/user_controller.rb
@@ -0,0 +1,4 @@
+class UserController < ApplicationController
+  def add
+  end
+end
---
+++ dolphin/dolphin.brain/test/controllers/role_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RoleControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
